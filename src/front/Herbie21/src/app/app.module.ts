6397559e16082bf7f1f6b06fbfe99b64cff14592
Gerson Scheffer <54009624+devscheffer@users.noreply.github.com>
2021-09-30 17:47:11 -0300

    Revert "Ajustes na tela de cadastro de pneus (#64)" (#65)
    This reverts commit 21db5c90da537a5d489830fae672ffce257d6f76.

---
+++ src/front/Herbie21/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { RouterModule } from '@angular/router';
+import { MenuPrincipalModule } from './modules/menu-principal/menu-principal.module';
+import { HomeComponent } from './core/components/home/home.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './core/components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { PneuModule } from './modules/pneu/pneu.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    MenuPrincipalModule,
+    CollapseModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    PneuModule
+  ],
+  exports: [HeaderComponent],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/front/Herbie21/src/app/modules/pneu/cadastra-pressao-pneu/cadastra-pressao-pneu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastra-pressao-pneu',
+  templateUrl: './cadastra-pressao-pneu.component.html',
+  styleUrls: ['./cadastra-pressao-pneu.component.scss']
+})
+export class CadastraPressaoPneuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
