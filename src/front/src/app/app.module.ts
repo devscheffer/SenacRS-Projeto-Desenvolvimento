350743272965feedb87cb1abf89344d9dd15cba7
Gian Alves <gian.alves@jrcomercio.com.br>
2021-12-02 12:50:43 -0300

    View, edit e delete

--- src/front/src/app/app.module.ts
+++ src/front/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core/core.module';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
-import { DialogBoxComponent } from './test-app/dialog-box/dialog-box.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
@@ -19,8 +18,7 @@ registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DialogBoxComponent
+    AppComponent
   ],
   imports: [
     AppRoutingModule,
